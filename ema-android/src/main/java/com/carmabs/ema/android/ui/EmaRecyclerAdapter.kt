52d495a7a7d0edb579e51d756c0bb006b614e071
Carlos <carlosmateo89@gmail.com>
2019-09-25 15:40:47 +0200

    Updated EmaRecyclerAdapter
    Support multiview. 
    No ViewHolder implementation necessary.

--- ema-android/src/main/java/com/carmabs/ema/android/ui/EmaRecyclerAdapter.kt
+++ ema-android/src/main/java/com/carmabs/ema/android/ui/EmaRecyclerAdapter.kt
@@ -1,6 +1,10 @@
 package com.carmabs.ema.android.ui
 
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import java.lang.RuntimeException
 
 /**
  * Adapter to implement the list view interface in recycler views
@@ -15,7 +19,7 @@ abstract class EmaRecyclerAdapter<I> : RecyclerView.Adapter<EmaViewHolder<I>>() 
     /**
      * Items to show in list
      */
-    abstract val listItems:MutableList<I>
+    abstract val listItems: MutableList<I>
 
     /**
      * Get the list size
@@ -33,11 +37,39 @@ abstract class EmaRecyclerAdapter<I> : RecyclerView.Adapter<EmaViewHolder<I>>() 
         holder.bind(listItems[position])
     }
 
+
+    /**
+     * Method called to set the ViewHolder. If different view holders are provided depending [viewType], use enableMultiViewHolder function
+     * @param parent holder where view components are implemented
+     * @param viewType of the item
+     */
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EmaViewHolder<I> {
+        return enableMultiViewHolder?.invoke(parent,viewType)
+                ?: EmaAdapterViewHolder(LayoutInflater.from(parent.context).inflate(layoutItemId
+                        ?: throw RuntimeException("Please provide the id for layoutItemId"), parent, false), viewType)
+    }
+
+    /**
+     * Resource id for item layout
+     */
+    protected abstract val layoutItemId: Int?
+
+    /**
+     * Method to set the view parameters from each item
+     */
+    protected abstract fun View.bind(item: I, viewType: Int)
+
+    /**
+     * Function to implement different viewHolders depending the viewType provided. Use only if you want to use different
+     * views depending the item provided
+     */
+    protected open val enableMultiViewHolder: ((view:ViewGroup,viewType:Int) -> EmaAdapterViewHolder)? = null
+
     /**
      * Update the adapter with a new list
      * @param listUpdate to set in the adapter
      */
-    fun updateList(listUpdate:List<I>){
+    fun updateList(listUpdate: List<I>) {
         listItems.apply {
             clear()
             addAll(listUpdate)
@@ -49,9 +81,9 @@ abstract class EmaRecyclerAdapter<I> : RecyclerView.Adapter<EmaViewHolder<I>>() 
      * Update the item of the list if exists. It must have the same memory reference
      * @param item to update
      */
-    fun updateItem(item : I){
+    fun updateItem(item: I) {
         val index = listItems.indexOf(item)
-        if(index>-1){
+        if (index > -1) {
             listItems[index] = item
             notifyItemChanged(index)
         }
@@ -61,7 +93,13 @@ abstract class EmaRecyclerAdapter<I> : RecyclerView.Adapter<EmaViewHolder<I>>() 
      * Add item to the list
      * @param item to add
      */
-    fun addItem(item: I,position:Int = listItems.size){
-        listItems.add(position,item)
+    fun addItem(item: I, position: Int = listItems.size) {
+        listItems.add(position, item)
+    }
+
+    protected open inner class EmaAdapterViewHolder(view: View, private val viewType: Int) : EmaViewHolder<I>(view) {
+        override fun bind(item: I) {
+            itemView.bind(item, viewType)
+        }
     }
 }
