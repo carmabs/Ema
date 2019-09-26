5251360a5a8c68bb54befd17568b5f50ce712a9b
Carlos <carlosmateo89@gmail.com>
2019-09-26 12:26:41 +0200

    Improved Sample Design

--- app/src/main/java/com/carmabs/ema/presentation/ui/user/EmaUserViewModel.kt
+++ app/src/main/java/com/carmabs/ema/presentation/ui/user/EmaUserViewModel.kt
@@ -1,9 +1,15 @@
 package com.carmabs.ema.presentation.ui.user
 
 import com.carmabs.ema.core.navigator.EmaNavigationState
+import com.carmabs.ema.core.state.EmaExtraData
 import com.carmabs.ema.presentation.base.BaseViewModel
 
 class EmaUserViewModel : BaseViewModel<EmaUserState, EmaNavigationState>() {
+
+    companion object{
+        const val SINGLE_EVENT_GROUP = 0
+        const val SINGLE_EVENT_USER = 1
+    }
 
     override fun onStartFirstTime(statePreloaded: Boolean) {
        val list =  createListItems()
@@ -14,15 +20,23 @@ class EmaUserViewModel : BaseViewModel<EmaUserState, EmaNavigationState>() {
 
     private fun createListItems():List<EmaUserItemModel> {
         return listOf(
-                EmaUserRightModel(1),
-                EmaUserLeftModel("Item 2"),
-                EmaUserLeftModel("Item 3"),
-                EmaUserRightModel(4)
+                EmaUserRightModel(3),
+                EmaUserLeftModel("Carmabs"),
+                EmaUserLeftModel("EMA"),
+                EmaUserRightModel(6)
         )
     }
 
 
     override fun createInitialViewState(): EmaUserState {
        return EmaUserState()
+    }
+
+    fun onActionUserClicked(item: EmaUserItemModel) {
+        val eventID = when(item.type){
+            EmaUserItemModel.Type.LEFT -> SINGLE_EVENT_USER
+            EmaUserItemModel.Type.RIGHT -> SINGLE_EVENT_GROUP
+        }
+        sendSingleEvent(EmaExtraData(eventID,item))
     }
 }
