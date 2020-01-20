7112e3a4736812b83659b2c45a3822ab871dac89
Carlos Mateo <carlosmateo89@gmail.com>
2020-01-20 17:06:23 +0100

    Updated TODO

--- app/src/main/java/com/carmabs/ema/presentation/dialog/simple/SimpleDialogData.kt
+++ app/src/main/java/com/carmabs/ema/presentation/dialog/simple/SimpleDialogData.kt
@@ -5,9 +5,10 @@ import com.carmabs.ema.core.dialog.EmaDialogData
 
 
 /**
- * TODO: Add a class header comment.
+ *  *<p>
+ * Copyright (c) 2020, Carmabs. All rights reserved.
+ * </p>
  *
-*
  *
  * @author <a href=“mailto:apps.carmabs@gmail.com”>Carlos Mateo</a>
  */
