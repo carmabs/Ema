3ddd3dc0a233d39ba5fa56b2ec80abf29b8b4b34
Carlos Mateo <apps.carmabs@gmail.com>
2021-03-09 03:21:35 +0100

    Added animation effects for ImageViews
    - Added crossfade transition effect
    - Added rotation bound effect

---
+++ ema-android/src/main/java/com/carmabs/ema/android/ConstantsAnimation.kt
@@ -0,0 +1,13 @@
+package com.carmabs.ema.android
+
+/**
+ * Created by Carlos Mateo Benito on 09/03/2021.
+ *
+ * <p>
+ * Copyright (c) 2021 by Carmabs. All rights reserved.
+ * </p>
+ *
+ * @author <a href=“mailto:apps.carmabs@gmail.com”>Carlos Mateo Benito</a>
+ */
+const val ANIMATION_DURATION = 250
+const val ANIMATION_OVERSHOOT = 10f
