bcbae02614f1a3de1cefa8d255ada99092a67001
Carlos Mateo <apps.carmabs@gmail.com>
2021-03-12 04:21:12 +0100

    Added first execution state check on fragment and activity
    - Added onFirstNormalExecution, onFirstAlternativeExecution, onFirstErrorExecution in EmaFragment and EmaActivity to check if it is first execution.
    - Handle the value to false in EmaView
    - Handle implementation of boolean variables in emaBooleanDelegate
    - Refactor emaStartTriggerDelegate

--- ema-android/src/main/java/com/carmabs/ema/android/ui/EmaActivity.kt
+++ ema-android/src/main/java/com/carmabs/ema/android/ui/EmaActivity.kt
@@ -11,6 +11,7 @@ import com.carmabs.ema.android.delegates.emaViewModelDelegate
 import com.carmabs.ema.android.extra.EmaActivityResult
 import com.carmabs.ema.android.viewmodel.EmaAndroidViewModel
 import com.carmabs.ema.android.viewmodel.EmaFactory
+import com.carmabs.ema.core.delegate.emaBooleanDelegate
 import com.carmabs.ema.core.navigator.EmaNavigationState
 import com.carmabs.ema.core.state.EmaBaseState
 import com.carmabs.ema.core.state.EmaState
@@ -56,6 +57,16 @@ abstract class EmaActivity<S : EmaBaseState, VM : EmaViewModel<S, NS>, NS : EmaN
     private val extraViewJobs: MutableList<Job> by lazy {
         mutableListOf<Job>()
     }
+
+    /**
+     * Determines first execution for each one of the state methods. EmaView determines when to set it to false.
+     */
+    final override var isFirstNormalExecution: Boolean by emaBooleanDelegate(true)
+
+    final override var isFirstAlternativeExecution: Boolean by emaBooleanDelegate(true)
+
+    final override var isFirstErrorExecution: Boolean by emaBooleanDelegate(true)
+
 
     /**
      * The view model of the fragment
