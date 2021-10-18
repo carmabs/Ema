b10b0043974242621b2f7bf7172b1e823c07e90b
Carlos Mateo <apps.carmabs@gmail.com>
2021-10-18 02:50:42 +0200

    Added EmaStringExtension
    - Added extension to replace last delimiter to a new string

--- ema-core/src/main/java/com/carmabs/ema/core/extension/EmaStringExtensions.kt
+++ ema-core/src/main/java/com/carmabs/ema/core/extension/EmaStringExtensions.kt
@@ -1,6 +1,7 @@
 package com.carmabs.ema.core.extension
 
-import com.carmabs.ema.core.constants.*
+import com.carmabs.ema.core.constants.INT_ZERO
+import com.carmabs.ema.core.constants.STRING_EMPTY
 
 /**
  * Created by Carlos Mateo Benito on 2019-11-24.
@@ -18,4 +19,10 @@ fun String?.checkNull(defaultValue: String = STRING_EMPTY): String {
 
 fun String.getFormattedString(vararg data: Any?): String {
     return String.format(this, *data)
+}
+
+fun String.replaceLast(delimiter: Char, newString: String): String {
+    val startString = substringBeforeLast(delimiter).trim()
+    val endString = substringAfterLast(delimiter).trim()
+    return startString + newString + endString
 }
