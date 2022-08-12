e2092191e4350191d49ab913206ecd541de549d6
Carlos Mateo <apps.carmabs@gmail.com>
2022-08-12 12:17:00 +0200

    Added new PermissionManager
    - Implemented permission manager based on ActivityResultContracts. It can get the user action directly without override onPermissionResult

---
+++ ema-core/src/main/java/com/carmabs/ema/core/manager/PermissionState.kt
@@ -0,0 +1,16 @@
+package com.carmabs.ema.core.manager
+
+/**
+ * Created by Carlos Mateo Benito on 2022-08-12.
+ *
+ * <p>
+ * Copyright (c) 2022 by Carmabs. All rights reserved.
+ * </p>
+ *
+ * @author <a href=“mailto:apps.carmabs@gmail.com”>Carlos Mateo Benito</a>
+ */
+enum class PermissionState {
+    GRANTED,
+    NOT_GRANTED,
+    NOT_GRANTED_SHOULD_EXPLAIN
+}
