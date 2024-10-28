6c2adefdd4ed0448f20d3b942c44d782efb8ae1a
Carlos Mateo <apps.carmabs@gmail.com>
2024-10-28 01:46:14 +0100

    Added EmaCancellable
    * To allow interface to return Cancellable actions.

---
+++ ema-core/src/main/java/com/carmabs/ema/core/utils/EmaCancellable.kt
@@ -0,0 +1,14 @@
+package com.carmabs.ema.core.utils
+
+/**
+ * Created by Carlos Mateo Benito on 23/10/24.
+ *
+ * <p>
+ * Copyright (c) 2024 by Carmabs. All rights reserved.
+ * </p>
+ *
+ * @author <a href=“mailto:apps.carmabs@gmail.com”>Carlos Mateo Benito</a>
+ */
+fun interface EmaCancellable {
+    fun cancel()
+}
